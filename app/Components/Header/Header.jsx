4fd14867dc587d80085308fef63e949ac6163b8a
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2025-11-18 20:51:30 +0530

    header component optimized

--- app/Components/Header/Header.jsx
+++ app/Components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import DateFormatter from "@/lib/DateFormatter";
 import { Settings } from "lucide-react";
 import { ShoppingBag } from "lucide-react";
-import React from "react";
+import React, { memo, useEffect, useState } from "react";
 
 const Header = ({ Name }) => {
+  const [CurrentDate, setCurrentDate] = useState(null);
+  useEffect(() => {
+    setCurrentDate(new Date());
+  }, []);
+
   return (
     <header className="px-4 py-3 bg-white w-full border-b shadow-md sticky top-0 z-10">
       <div className="text-center">
@@ -12,10 +17,12 @@ const Header = ({ Name }) => {
           <h1 className="font-bold text-2xl"> {Name?.toUpperCase() || ""}</h1>
           <Settings className="w-6 h-6" />
         </div>
-        <div>{DateFormatter({ date: new Date() })}</div>
+        <div>
+          <DateFormatter date={CurrentDate} />
+        </div>
       </div>
     </header>
   );
 };
 
-export default Header;
+export default memo(Header);
